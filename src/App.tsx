2d532b819ddc54a0d0ceb9122cde181d86c4dc3d
Yunseonghwan <sunghwan0208@naver.com>
2021-07-11 17:10:56 +0900

    tailwind css setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div>hello tailwind</div>;
+  return (
+    <div className=" min-h-full bg-red-500 h-full">
+      <h1 className=" text-xl text-white ">Hello Tailwind</h1>
+    </div>
+  );
 }
 
 export default App;
